bf5073293e9bbf88d49d446592bd36f04fad0895
mdshajibazher <mdshajibazher@gmail.com>
2020-03-19 22:36:09 +0600

    Welcome Page Particle Js Problem Fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,88 +6,27 @@
 
         <title>Laravel</title>
         
-        
-    
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #EA2027;
-                color: #fff;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .wrapper{
-                position: absolute;
-                left: 0;
-                top: 0;
-                right: 0;
-                bottom: 0;
-            }
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 60px;
-            }
-
-            .links > a {
-                color: #ffffff;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" href="{{asset('public/css/welcome.css')}}">
     </head>
-    <body id="particles-js">
-        <div  class="wrapper">
-        <div class="flex-center position-ref full-height">
+    <body >
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">Student Login</a>
                         <a href="{{ route('admin.login') }}">Admin Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">Student Register</a>
                         @endif
                     @endauth
                 </div>
             @endif
 
+        <div id="particles-js">
+            <div class="wrapper">
+        <div class="flex-center full-height">
             <div class="content">
                  <img src="{{asset('public/image/logo.png')}}" alt="">
                 <div class="title m-b-md">
@@ -96,6 +35,7 @@
                 </div>
             </div>
         </div>
+    </div>
         </div>
         <script src="{{asset('public/js/jquery.js')}}"></script>
         <script src="{{asset('public/js/particle.js')}}"></script>
